47743f04ae57992cd582cd343139e388252a352d
Umidjon Abduraxmonov <u.abduraxmanov@student.pdp.university>
2026-07-03 20:10:56 +0500

    Refactor TwoSum to improve variable naming and logic
    Refactor TwoSum method to use seenNumbers dictionary for tracking indices.

---
+++ 0001-two-sum/hash-map.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+    Dictionary<int, int> seenNumbers = new Dictionary<int, int>();
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        int complement = target - nums[i];
+
+        if (seenNumbers.ContainsKey(complement))
+        {
+            return new int[] { seenNumbers[complement], i };
+        }
+
+        seenNumbers[nums[i]] = i;
+    }
+
+    return new int[] {};
+    }
+}
